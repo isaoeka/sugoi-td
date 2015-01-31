4cb8090bbccb8bb9055394786268a5bbc475ee4d
kono <kono@mfac.jp>
2015-01-31 18:05:10 +0900

    geme scene added

--- Classes/MainGameScene.cpp
+++ Classes/MainGameScene.cpp
@@ -1,15 +1,14 @@
-#include "StartPageScene.h"
 #include "MainGameScene.h"
 
 USING_NS_CC;
 
-Scene* StartPageScene::createScene()
+Scene* MainGameScene::createScene()
 {
     // 'scene' is an autorelease object
     auto scene = Scene::create();
 
     // 'layer' is an autorelease object
-    auto layer = StartPageScene::create();
+    auto layer = MainGameScene::create();
 
     // add layer as a child to scene
     scene->addChild(layer);
@@ -19,7 +18,7 @@ Scene* StartPageScene::createScene()
 }
 
 // on "init" you need to initialize your instance
-bool StartPageScene::init()
+bool MainGameScene::init()
 {
     //////////////////////////////
     // 1. super init first
@@ -33,38 +32,15 @@ bool StartPageScene::init()
     /////////////////////////////
     // 2. add a menu item with "X" image, which is clicked to quit the program
     //    you may modify it.
-
     // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-        "enemy/1.png",
-        "enemy/2.png",
-        CC_CALLBACK_1(StartPageScene::myCallback, this));
-
-    closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width / 2,
-        origin.y + closeItem->getContentSize().height / 2));
-
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
 
     auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-
-    // position the label on the center of the screen
     label->setPosition(Vec2(origin.x + visibleSize.width / 2,
         origin.y + visibleSize.height - label->getContentSize().height));
-
-    // add the label as a child to this layer
     this->addChild(label, 1);
 
     // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
+    auto sprite = Sprite::create("enemy/3.png");
 
     // position the sprite on the center of the screen
     sprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
@@ -72,23 +48,12 @@ bool StartPageScene::init()
     // add the sprite as a child to this layer
     this->addChild(sprite, 0);
 
+    /////////////////////////////
+    // 3. add your codes below...
+
     return true;
 }
 
 //-------------------------------------------------------------------
 #pragma mark -  callback
 //-------------------------------------------------------------------
-
-void StartPageScene::myCallback(Ref* pSender)
-{
-    log("タップされました。");
-    gameStart(1);
-}
-
-void StartPageScene::gameStart(int difficulty)
-{
-    auto scene = MainGameScene::createScene();
-    // 0.5秒かけてホワイトアウトしてタイトルに移動する
-    TransitionFade* fade = TransitionFade::create(0.5f, scene, Color3B::WHITE);
-    Director::getInstance()->replaceScene(fade);
-}
